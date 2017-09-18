37f64f206f33b2d4e027ba01d6e0cd4b5f657242
oj3 <30613032+oj3@users.noreply.github.com>
2017-09-19 00:08:16 +0900

    delete all
    for reconstruction

--- ch6.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.append(os.pardir)
-import numpy as np
-from ch4 import numerical_gradient
-from layers import *
-from optimizer import *
-from collections import OrderedDict
-from dataset.mnist import load_mnist
- 
